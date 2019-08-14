a53bc9943fb2199c8e62b2f5352eb2a56c97bf93
dieghoatc <dieghoatc@gmail.com>
2019-08-14 13:37:33 -0500

    Aplicando estilos a ReactJS

--- src/index.js
+++ src/index.js
@@ -6,20 +6,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const element = <h1>Dieghoatc</h1>;
-const sum = () => 8 * 9;
-function resta() {
-  return 9 - 1;
-}
-const name = "Dieghoatc";
-const jsx = <h1>Hola soy, {resta()}</h1>;
-const jsxx = (
-  <div>
-    <h1>Hola, soy Diego</h1>
-    <p>Soy ingeniero Fronted</p>
-  </div>
-);
+//npm install bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import Badge from "./components/Badge";
+
 const container = document.getElementById("app");
 
 //render optiene dos argumentos(El que, y el donde)
-ReactDOM.render(jsxx, container);
+ReactDOM.render(<Badge />, container);
