20ae33e303c087b2a27a17d01ec7dfeddd29ba80
dieghoatc <dieghoatc@gmail.com>
2019-08-15 11:27:22 -0500

    Levantamiento del estado, permite comparti la informacion del formulario a mas de un componente

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -4,9 +4,9 @@ class BadgeForm extends React.Component {
   //es necesario inicializar los estados, puede ser en cualquier liena
   //setStatey state con partes basicas en todo componente,
 
-  state = {
-    jobTitle: "Designer"
-  };
+  //   state = {
+  //     jobTitle: "Designer"
+  //   };
 
   handleChenge = e => {
     // console.log({
@@ -38,51 +38,51 @@ class BadgeForm extends React.Component {
           <div className="form-group">
             <label>First name</label>
             <input
-              onChange={this.handleChenge}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
-              name="firstname"
-              value={this.state.firstName}
+              name="firstName"
+              value={this.props.formValues.firstName}
             />
           </div>
           <div className="form-group">
             <label>Last name</label>
             <input
-              onChange={this.handleChenge}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
-              name="lastname"
-              value={this.state.lastName}
+              name="lastName"
+              value={this.props.formValues.lastName}
             />
           </div>
           <div className="form-group">
             <label>Email</label>
             <input
-              onChange={this.handleChenge}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="email"
-              value={this.state.email}
+              value={this.props.formValues.email}
             />
           </div>
           <div className="form-group">
             <label>Job Title</label>
             <input
-              onChange={this.handleChenge}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="jobTitle"
-              value={this.state.jobTitle}
+              value={this.props.formValues.jobTitle}
             />
           </div>
           <div className="form-group">
-            <label>Teitter</label>
+            <label>Twitter</label>
             <input
-              onChange={this.handleChenge}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               name="twitter"
-              value={this.state.twitter}
+              value={this.props.formValues.twitter}
             />
           </div>
           <button onClick={this.handleClick} className="btn btn-primary">
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,27 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      jobTitle: "",
+      twitter: "",
+      email: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        //Caen todos los valores que estaban en this.state.form
+        //crando un nuevo objeto o reesribiendo los valores del formulario
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div>
@@ -18,15 +39,19 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Anakyn"
-                lastName="Skywalker"
-                twitter="Dieghoatc"
-                jobTitle="Frontend Enginner"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://s.gravatar.com/avatar/c1d0a85cc9ed33f1839cf20d818a32a5?s=80"
               />
             </div>
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
