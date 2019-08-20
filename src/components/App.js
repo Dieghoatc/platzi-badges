dcfe6eaf23618df26b0b3b6977e146be104c6809
dieghoatc <dieghoatc@gmail.com>
2019-08-20 17:20:24 -0500

    Detalles del badge

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import BadgeNew from "../pages/BadgeNew";
 import Badges from "../pages/Badges";
 import Home from "../pages/Home";
+import BadgeDetails from "../pages/BadgeDetails";
 import BadgeEdit from "../pages/BadgeEdit";
 import NotFound from "../pages/NotFound";
 import Layout from "./Layout";
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId/" component={BadgeDetails} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
