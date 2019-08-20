837d5e49710e28ee0678b2c1bf7bb0958cf29092
dieghoatc <dieghoatc@gmail.com>
2019-08-20 12:52:18 -0500

    Manejo de estados del Post

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,10 +4,14 @@ import "./styles/BadgeNew.css";
 import header from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
+
+import PageLoading from "../components/PageLoading";
 import api from "../api";
 
 class BadgeNew extends React.Component {
   state = {
+    loading: false,
+    error: null,
     form: {
       firstName: "",
       lastName: "",
@@ -35,11 +39,17 @@ class BadgeNew extends React.Component {
     try {
       await api.badges.create(this.state.form);
       this.setState({ loading: false });
+
+      //redireccion del usuario a:
+      this.props.history.push("/badges");
     } catch (error) {
       this.setState({ loading: false, error: error });
     }
   };
   render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
     return (
       <div>
         <div className="BadgeNew__hero">
@@ -62,6 +72,7 @@ class BadgeNew extends React.Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 formValues={this.state.form}
+                error={this.state.error}
               />
             </div>
           </div>
