c45b723d2b0d6ba22467ed246797db255efc8aa4
dieghoatc <dieghoatc@gmail.com>
2019-08-20 22:47:54 -0500

    Search filter, utilizando hooks

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -27,43 +27,83 @@ class BadgesListItem extends React.Component {
     );
   }
 }
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState("");
 
-class BadgesList extends React.Component {
-  render() {
-    if (this.props.badges.length === 0) {
-      return (
-        <div>
-          <h3>No se econtro ningun badge</h3>
-          <Link className="btn btn-primary" to="/badges/new">
-            Create New Badge
-          </Link>
-        </div>
-      );
-    }
+  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const results = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(results);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+  if (filteredBadges.length === 0) {
     return (
-      <div className="BadgesList">
-        <ul className="list-unstyled">
-          {/* Para que por cada dato aparesca un componente. Por cada uno de los
+      <div>
+        <div className="form-group">
+          <label>Filter Badges</label>
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+        <h3>No se econtro ningun badge</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create New Badge
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <div className="BadgesList">
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input
+          type="text"
+          className="form-control"
+          value={query}
+          onChange={e => {
+            setQuery(e.target.value);
+          }}
+        />
+      </div>
+      <ul className="list-unstyled">
+        {/* Para que por cada dato aparesca un componente. Por cada uno de los
           elementos que tengo en data, quiero convertirlos de un objeto a un
           elemento. La funcion sobre los arreglos es (.map) la cual recibe una
           funcion como argumento y tiene que regresar un valor
           cada hijo en una lista, tiene que tener un prop unico llamado key valor unico ayuda a react para determinar si hubo un cambio al volver a hacer render */}
-          {this.props.badges.map(badge => {
-            return (
-              <li key={badge.id}>
-                <Link
-                  className="text-reset text-decoration-none"
-                  to={`/badges/${badge.id}`}
-                >
-                  <BadgesListItem badge={badge} />
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+        {filteredBadges.map(badge => {
+          return (
+            <li key={badge.id}>
+              <Link
+                className="text-reset text-decoration-none"
+                to={`/badges/${badge.id}`}
+              >
+                <BadgesListItem badge={badge} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default BadgesList;
